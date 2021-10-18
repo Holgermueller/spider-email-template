846b0c0005250c368775aff8b56bb1918790305a
Holger Mueller <9jholger@gmail.com>
2021-10-18 11:29:52 -0500

    finished scaffold login registration forms

--- src/components/HomeComponents/Login.vue
+++ src/components/HomeComponents/Login.vue
@@ -4,7 +4,44 @@
       Log in
     </v-expansion-panel-header>
     <v-expansion-panel-content>
-      form here
+      <v-card elevation="0">
+        <v-card-text>
+          <v-form ref="form" id="loginForm" class="log-in-form">
+            <v-text-field
+              type="email"
+              id="email"
+              value="email"
+              v-model="email"
+              label="Email"
+              outlined
+              clearable
+              prepend-icon="mdi-email"
+            ></v-text-field>
+
+            <v-text-field
+              type="password"
+              id="password"
+              value="password"
+              v-model="password"
+              label="Password"
+              outlined
+              clearable
+              prepend-icon="mdi-lock"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn elevation="0">
+            <v-icon left>mdi-cancel</v-icon>
+            Cancel</v-btn
+          >
+          <v-spacer></v-spacer>
+          <v-btn elevation="0"
+            >Submit
+            <v-icon right>mdi-login</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-expansion-panel-content>
   </v-expansion-panel>
 </template>
@@ -13,6 +50,15 @@
 export default {
   name: "LogInForm",
 
-  data: () => ({}),
+  data: () => ({
+    password: "",
+    email: "",
+  }),
 };
 </script>
+
+<style scoped>
+.v-expansion-panel::before {
+  box-shadow: none;
+}
+</style>
--- src/components/HomeComponents/Register.vue
+++ src/components/HomeComponents/Register.vue
@@ -5,7 +5,52 @@
     </v-expansion-panel-header>
 
     <v-expansion-panel-content>
-      Form here...
+      <v-card elevation="0">
+        <v-card-text>
+          <v-text-field
+            type="email"
+            id="email"
+            v-model="email"
+            value="email"
+            placeholder="Email"
+            prepend-icon="mdi-email"
+            clearable
+            outlined
+          ></v-text-field>
+          <v-text-field
+            type="password"
+            id="password"
+            v-model="password"
+            value="password"
+            placeholder="Password"
+            prepend-icon="mdi-lock"
+            clearable
+            outlined
+          ></v-text-field>
+          <v-text-field
+            type="password"
+            id="confirmPassword"
+            v-model="confirmPassword"
+            value="confirmPassword"
+            placeholder="Confirm password"
+            prepend-icon="mdi-lock"
+            clearable
+            outlined
+          ></v-text-field>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn elevation="0" dark>
+            <v-icon left>mdi-cancel</v-icon>
+            Cancel</v-btn
+          >
+          <v-spacer></v-spacer>
+          <v-btn elevation="0" dark
+            >Submit
+            <v-icon right>mdi-login</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-expansion-panel-content>
   </v-expansion-panel>
 </template>
@@ -14,6 +59,16 @@
 export default {
   name: "RegistrationForm",
 
-  data: () => ({}),
+  data: () => ({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  }),
 };
 </script>
+
+<style scoped>
+.v-expansion-panel::before {
+  box-shadow: none;
+}
+</style>
