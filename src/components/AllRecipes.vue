463af9c164da687ab73dffc6334cbd0d1fb509db
Holger Mueller <9jholger@gmail.com>
2021-10-27 21:29:03 -0500

    built card for recipes

--- src/components/AllRecipes.vue
+++ src/components/AllRecipes.vue
@@ -2,20 +2,21 @@
   <div>
     <RecipesListFilter />
 
-    <h1>
-      All Recipes here
-    </h1>
+    <RecipesCard />
   </div>
 </template>
 
 <script>
 import RecipesListFilter from "./AllRecipesComponents/RecipesFilter.vue";
 
+import RecipesCard from "./AllRecipesComponents/RecipesCard.vue";
+
 export default {
   name: "AllRecipesPage",
 
   components: {
     RecipesListFilter,
+    RecipesCard,
   },
 
   data: () => ({}),
---
+++ src/components/AllRecipesComponents/RecipesCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-card class="recipe-card">
+      <v-card-title></v-card-title>
+
+      <v-card-text></v-card-text>
+
+      <v-card-actions>
+        <v-chip>
+          ingredients
+        </v-chip>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecipeCard",
+
+  data: () => ({}),
+};
+</script>
+
+<style scoped>
+.recipe-card {
+  width: 75%;
+  margin: 4px auto;
+}
+</style>
