7383f4f715acb0290327a1887446d9ae3b553b13
holgermueller <9jholger@gmail.com>
2023-09-10 15:56:52 -0500

    got cube to turn

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 "use strict";
 
+const cube = document.querySelector(".cube");
+let currentClass = "";
+
 const noGo = () => {
-  console.log("click");
+  const sideToShow = document.getElementById("cube");
+  if (currentClass === "show-front") {
+    cube.classList.remove(currentClass);
+  }
+  sideToShow.classList.add("show-left");
+
+  currentClass = sideToShow;
 };
 
 const yesGo = () => {
-  console.log("yes");
+  const sideToShow = document.getElementById("cube");
+  if (currentClass === "show-front") {
+    cube.classList.remove(currentClass);
+  }
+  sideToShow.classList.add("show-right");
+
+  currentClass = sideToShow;
 };
